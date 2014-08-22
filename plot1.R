a12acd4e01638f925b9ee14db125f3cf4030d250
Gustavo Jimenez-Maggiora <gajimenezmaggiora@gmail.com>
2014-08-22 12:55:27 -0700

    Added comments

--- plot1.R
+++ plot1.R
@@ -1,3 +1,8 @@
+# Question:
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all
+# sources for each of the years 1999, 2002, 2005, and 2008.
+
 # Check to see if our data files are present in the cwd
 if( !all( file.exists('./Source_Classification_Code.rds', './summarySCC_PM25.rds')) ) {
   stop('Error: Data files not found!!!')
