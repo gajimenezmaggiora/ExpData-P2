7edc19f46aa3ede0ba9aa1ef066c7cd880510112
Gustavo Jimenez-Maggiora <gajimenezmaggiora@gmail.com>
2014-08-22 08:46:56 -0700

    Initial upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+# Check to see if our data files are present in the cwd
+if( !all( file.exists('./Source_Classification_Code.rds', './summarySCC_PM25.rds')) ) {
+  stop('Error: Data files not found!!!')
+}
+
+# Read data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
