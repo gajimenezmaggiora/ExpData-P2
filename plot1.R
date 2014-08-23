e57d4ac3057c71197fb01a337e516d1970558255
Gustavo Jimenez-Maggiora <gajimenezmaggiora@gmail.com>
2014-08-22 21:40:16 -0700

    Improved script.  Added png version of plot

--- plot1.R
+++ plot1.R
@@ -11,3 +11,22 @@ if( !all( file.exists('./Source_Classification_Code.rds', './summarySCC_PM25.rds
 # Read data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+# Get emissions and dates
+yrs <- NEI$year
+pm <- NEI$Emissions
+
+# Calculate total emissions per year
+emissions <- tapply(pm, yrs, sum, na.rm=T)
+years <- as.numeric(names(emissions))
+
+# Generate plot
+
+plot(years, emissions)
+abline(lm(emissions ~ years), col='red')
+title('Total Emissions from PM2.5 in the US')
+
+# Generate png file
+dev.copy(png, file = "./plot1.png")
+dev.off()
+
