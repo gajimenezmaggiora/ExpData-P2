ea59214d0dcfc73073030a07158be1ff3d91783a
Gustavo Jimenez-Maggiora <gajimenezmaggiora@gmail.com>
2014-08-23 00:47:53 -0700

    Updated plot3.R

--- plot3.R
+++ plot3.R
@@ -13,3 +13,36 @@ if( !all( file.exists('./Source_Classification_Code.rds', './summarySCC_PM25.rds
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+# Load ggplot2 library
+library(ggplot2)
+
+# Subset data
+NEIsub <- subset(NEI, fips=='24510')
+
+# Get emissions and dates
+yrs <- NEIsub$year
+pm <- NEIsub$Emissions
+pts <- NEIsub$type
+
+# Create a data frame
+df <- data.frame(years=yrs, emissions=pm, points=pts)
+
+# Aggregate the emission date by years and sources
+df2<-with(df, aggregate(emissions, by=list(years,points), sum, na.rm=T))
+names(df2)<-c('year','type','emissions')
+
+# Create plot
+gplot <- qplot(year, emissions, data=df2, color=type, geom=c('point', 'smooth'),
+method='lm',
+main='Total Emissions from PM2.5 in Baltimore City, MD by Source',
+xlab='Year',
+ylab='Total Emissions') + facet_wrap(~type, nrow=2)
+
+# Print plot
+print(gplot)
+
+# Generate png file
+dev.copy(png, file = "./plot3.png")
+dev.off()
+
+
